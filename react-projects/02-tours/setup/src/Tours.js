33d70e23693ce76157610456191f002cf53e21f7
Prashant Kumar <kr.prashantjsr@gmail.com>
2022-05-26 02:16:00 +0530

    tours done

---
+++ react-projects/02-tours/setup/src/Tours.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Tour from './Tour';
+const Tours = ({tours,removeTour}) => {
+  console.log(tours);
+  return (
+    <section>
+      <div className="title">
+        <h2>Our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          console.log(tour);
+          return(
+            <Tour key={tour.id} {...tour} removeTour={removeTour}></Tour>
+          )
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Tours;
