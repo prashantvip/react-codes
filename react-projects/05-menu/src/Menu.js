427c3addce0aab4579e586a71b85c972d2e61158
Prashant Kumar <kr.prashantjsr@gmail.com>
2022-05-28 00:20:55 +0530

    menu done

--- react-projects/05-menu/src/Menu.js
+++ react-projects/05-menu/src/Menu.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-const Menu = ({title,category,price,img,desc}) => {
+const Menu = ({items}) => {
   return (
-    <div>
-      <img src={img} alt="" />
-      <p>{desc}</p>
+    <div className="section-center">
+      {items.map((item) => {
+        const {id,title,img,desc,price} = item;
+        return(
+          <article key={id} className='menu-item'>
+            <img src={img} alt={title} className='photo' />
+            <div className="item-info">
+              <header>
+                <h4>{title}</h4>
+                <h4 className="price">{price}</h4>
+              </header>
+              <p className="item-text">{desc}</p>
+            </div>
+          </article>
+        )
+      })}
     </div>
   );
 };
