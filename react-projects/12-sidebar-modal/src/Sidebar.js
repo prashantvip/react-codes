b66e17e7fb3574ad052ad06f4d9e00b4f0762868
Prashant Kumar <kr.prashantjsr@gmail.com>
2022-06-21 08:02:56 +0530

    modal done

---
+++ react-projects/12-sidebar-modal/src/Sidebar.js
@@ -0,0 +1,46 @@
+import React,{useContext} from 'react'
+import logo from './logo.svg'
+import { FaTimes } from 'react-icons/fa'
+import { social, links } from './data'
+import { AppContext } from './context'
+
+const Sidebar = () => {
+  const {isSidebarOpen,closeSidebar} = useContext(AppContext);
+  return (
+    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar': 'sidebar'}`}>
+      <div className="sidebar-header">
+        <img src={logo} alt="coding addict" className='logo' />
+        <button className='close-btn' onClick={closeSidebar}>
+          <FaTimes/>
+        </button>
+      </div>
+      <ul className='links'>
+        {links.map((link) => {
+          const {id,url,text,icon} = link
+          return(
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+      <ul className="social-icons">
+        {social.map((link) => {
+          const {id,url,icon} = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </aside>
+  )
+}
+
+export default Sidebar
