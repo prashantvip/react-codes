36737c0971e2789d5025a9a56e77f4d4246a8bd9
Prashant Kumar <kr.prashantjsr@gmail.com>
2022-07-25 20:48:35 +0530

    cocktailsdb done

---
+++ react-projects/cocktailsdb/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <section className="section about-section">
+        <h1 className='section-title'>about us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia harum fuga error veritatis dignissimos expedita, odit sint, corrupti cum pariatur sunt! Unde, porro aliquid. Aspernatur iste repellendus dolore qui aliquid, minus nobis cum quisquam consequuntur nemo fugiat atque aperiam suscipit!</p>
+      </section>
+    </div>
+  )
+}
+
+export default About
---
+++ react-projects/cocktailsdb/src/pages/Error.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <div>
+      <section className='section error-page'>
+        <div className="error-container">
+          <h1>oops! it's a dead end</h1>
+          <Link to="/" className='btn btn-primary'>
+            back to home
+          </Link>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Error
